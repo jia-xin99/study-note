9577ec123d37f45fcf634eb045e79b3e8b25e7b5
jiaxin <3225465447@qq.com>
2023-02-24 22:37:26 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,12 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig } from 'vuepress';
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   base: 'study-note',
-  description: '这是我的第一个 VuePress 站点'
+  description: '这是我的第一个 VuePress 站点',
+  theme: hopeTheme({
+    // 此处放置主题配置
+  }),
 })
