0d67b3d758abad80401de2a39902745222044df6
jiaxin <3225465447@qq.com>
2023-02-24 23:17:08 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
+  title: '学习笔记',
   base: 'study-note',
   description: '这是我的第一个 VuePress 站点',
   theme: hopeTheme({
